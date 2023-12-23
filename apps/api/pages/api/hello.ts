f8c3a1d8cffc4aaf6bba658093214c429fd66285
Intizar Tashov <intizar.tashov22@gmail.com>
2023-12-23 23:56:58 +0300

    add cors middleware to api

--- apps/api/pages/api/hello.ts
+++ apps/api/pages/api/hello.ts
@@ -1,12 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import Cors from "cors";
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function,
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 type ResponseData = {
   message: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>,
 ) {
-  res.status(200).json({ message: "Hello from Next.js!" });
+  await runMiddleware(req, res, cors);
+  res.status(200).json({ message: "Yo!" });
 }
