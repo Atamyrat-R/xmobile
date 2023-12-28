6a148eb77c15e25fa0df90ba96bbfa01a54b7d80
Hudayar-T <tash.hudayar@gmail.com>
2023-12-28 21:48:59 +0300

    build skeleton of dashboard

--- apps/client/app/lib/data.ts
+++ apps/client/app/lib/data.ts
@@ -30,14 +30,14 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
+    // console.log("Fetching revenue data...");
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const { success, data }: { success: boolean; data: Revenue[] } = await (
       await fetch(REVENUE, { cache: "no-store" })
     ).json();
 
-    // console.log('Data fetch completed after 3 seconds.');
+    // console.log("Data fetch completed after 3 seconds.");
 
     return data;
   } catch (error) {
